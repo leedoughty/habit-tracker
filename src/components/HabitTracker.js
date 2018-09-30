9cf91c0b39615b26e697c7465c2e35c018d8a1b6
Lee Doughty <32392044+leedoughty@users.noreply.github.com>
2018-09-30 11:06:20 +0100

    Addition of habit tracker component

---
+++ src/components/HabitTracker.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../stylesheets/ui.scss'
+
+const HabitTracker = React.createClass({
+  render() {
+      return (
+        <div className="habit-tracker">
+          <div className="total-days">
+          </div>
+        </div>
+      )
+  }
+})
