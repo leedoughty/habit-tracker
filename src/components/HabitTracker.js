b4e5ea51790e250eab20c9d098bee2207a722f43
Lee Doughty <32392044+leedoughty@users.noreply.github.com>
2018-09-30 17:32:18 +0100

    Added function for calculating goal progress

--- src/components/HabitTracker.js
+++ src/components/HabitTracker.js
@@ -1,11 +1,35 @@
 import React from 'react'
 import '../stylesheets/ui.scss'
 
-const HabitTracker = React.createClass({
+export const HabitTracker = React.createClass({
+  percentToDecimal(decimal) {
+    return ((decimal * 100) + '%')
+  },
+  calcGoalProgress(total, goal) {
+    return this.percentToDecimal(total/goal)
+  },
   render() {
       return (
         <div className="habit-tracker">
           <div className="total-days">
+            <span>{this.props.total}</span>
+            <span>days</span>
+          </div>
+          <div className="Yes">
+            <span>{this.props.yes}</span>
+            <span>days</span>
+          </div>
+          <div className="No">
+            <span>{this.props.no}</span>
+            <span>days</span>
+          </div>
+          <div>
+            <span>
+              {this.calcGoalProgress(
+                this.props.total,
+                this.props.goal
+              )}
+            </span>
           </div>
         </div>
       )
