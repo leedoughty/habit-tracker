c5da04eeeea104446f81299e119ab7eeaaca2bf4
Lee Doughty <32392044+leedoughty@users.noreply.github.com>
2018-10-01 18:53:59 +0100

    Destructuring component

--- src/components/HabitTracker.js
+++ src/components/HabitTracker.js
@@ -1,37 +1,35 @@
 import { Component } from 'react'
 import '../stylesheets/ui.scss'
 
-export class HabitTracker extends Component{
-  percentToDecimal(decimal) {
-    return ((decimal * 100) + '%')
-  }
-  calcGoalProgress(total, goal) {
-    return this.percentToDecimal(total/goal)
-  }
-  render() {
-      return (
-        <div className="habit-tracker">
-          <div className="total-days">
-            <span>{this.props.total}</span>
-            <span>days</span>
-          </div>
-          <div className="Yes">
-            <span>{this.props.yes}</span>
-            <span>days</span>
-          </div>
-          <div className="No">
-            <span>{this.props.no}</span>
-            <span>days</span>
-          </div>
-          <div>
-            <span>
-              {this.calcGoalProgress(
-                this.props.total,
-                this.props.goal
-              )}
-            </span>
-          </div>
-        </div>
-      )
-  }
+const percentToDecimal = (decimal) => {
+  return ((decimal * 100) + '%')
 }
+
+const calcGoalProgress = (total, goal) => {
+  return percentToDecimal(total/goal)
+}
+
+export const HabitTracker = ({total, yes, no, goal}) => (
+      <div className="habit-tracker">
+        <div className="total-days">
+          <span>{total}</span>
+          <span>days</span>
+        </div>
+        <div className="Yes">
+          <span>{yes}</span>
+          <span>days</span>
+        </div>
+        <div className="No">
+          <span>{no}</span>
+          <span>days</span>
+        </div>
+        <div>
+          <span>
+            {calcGoalProgress(
+              total,
+              goal
+            )}
+          </span>
+        </div>
+      </div>
+)
