80bdaf9f4029048ab418f5346fccbefd11cec756
Lee Doughty <32392044+leedoughty@users.noreply.github.com>
2018-10-06 11:30:35 +0100

    Refactorization of tracker, list, and row components

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,38 @@
-import { createClass } from 'react'
+import { Component } from 'react'
 import { HabitTrackerList } from './HabitTrackerList'
 import { HabitTracker } from './HabitTracker'
 
-export const App = createClass({
-  getInitialState() {
-    return {
-        allHabitDays: [
-				{
-					activity: "Meditation",
-					date: new Date("1/2/2018"),
-					yes: true,
-					no: false
-				},
-				{
-					activity: "Practice Japanese",
-					date: new Date("3/28/2018"),
-					yes: false,
-					no: true
-				},
-				{
-					activity: "Code",
-					date: new Date("4/28/2018"),
-					yes: true,
-					no: false
-				}
-			]
+export class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      allHabitDays: [
+      {
+        activity: "Meditation",
+        date: new Date("1/2/2018"),
+        yes: true,
+        no: false
+      },
+      {
+        activity: "Practice Japanese",
+        date: new Date("3/28/2018"),
+        yes: false,
+        no: true
+      },
+      {
+        activity: "Code",
+        date: new Date("4/28/2018"),
+        yes: true,
+        no: false
+      }
+    ]
     }
-  },
+  }
   countDays(filter) {
     const { allHabitDays } = this.state
     return allHabitDays.filter(
         (day) => (filter) ? day[filter] : day).length
-  },
+  }
   render() {
     return (
       <div className="app">
@@ -42,4 +43,4 @@ export const App = createClass({
       </div>
     )
   }
-})
+}
