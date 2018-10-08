bac77b9eb30734f028f303f0b4335592c3e67af8
Lee Doughty <32392044+leedoughty@users.noreply.github.com>
2018-10-08 11:01:23 +0100

    Refactorization of form

--- src/components/AddDayForm.js
+++ src/components/AddDayForm.js
@@ -1,39 +1,56 @@
-import { PropTypes, Component } from 'react'
+import { PropTypes } from 'react'
 
-export class AddDayForm extends Component {
-  render() {
+export const AddDayForm = ({ activity, date, yes, no, onNewDay }) => {
 
-    const { activity, date, yes, no } = this.props
-
-    return (
-      <form className="add-day-form">
-        <label htmlFor="activity">Activity Name</label>
-        <input id="activity"
-               type="text"
-               required
-               defaultValue={activity}/>
-
-        <label htmlFor="date">Date</label>
-        <input id="date"
-               type="date"
-               required
-               defaultValue={date}/>
-
-        <div>
-          <input id="yes"
-                 type="checkbox"
-                 defaultChecked={yes} />
-          <label htmlFor="yes">Yes</label>
-        </div>
-        <div>
-          <input id="no"
-                 type="checkbox"
-                 defaultChecked={no} />
-          <label htmlFor="no">No</label>
-        </div>
-      </form>
-    )
+  let _activity, _date, _yes, _no
+  const submit = (e) => {
+    e.preventDefault()
+    onNewDay({
+      activity: _activity.value,
+      date: _date.value,
+      yes: _yes.checked,
+      no: _no.checked
+    })
+    _activity.value = ''
+    _date.value = ''
+    _yes.checked = false
+    _no.checked = false
   }
+
+  return (
+    <form onSubmit={submit} className="add-day-form">
+      <label htmlFor="activity">Activity Name</label>
+      <input id="activity"
+             type="text"
+             required
+             defaultValue={activity}
+             ref={input => _activity = input}/>
+
+      <label htmlFor="date">Date</label>
+      <input id="date"
+             type="date"
+             required
+             defaultValue={date}
+             ref={input => _date = input}/>
+      <div>
+        <input id="yes"
+               type="checkbox"
+               defaultChecked={yes}
+               ref="yes"
+               ref={input => _yes = input}/>
+        <label htmlFor="yes">Yes</label>
+      </div>
+      <div>
+        <input id="no"
+               type="checkbox"
+               defaultChecked={no}
+               ref="no"
+               ref={input => _no = input}/>
+        <label htmlFor="no">No</label>
+      </div>
+      <button>Add Day</button>
+    </form>
+  )
 }
 
 AddDayForm.defaultProps = {
