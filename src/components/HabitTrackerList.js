0ef1effbd044d5a418ea1855a9415b69c033d739
Lee Doughty <32392044+leedoughty@users.noreply.github.com>
2018-10-09 19:06:35 +0100

    Installation of react router

--- src/components/HabitTrackerList.js
+++ src/components/HabitTrackerList.js
@@ -3,20 +3,43 @@ import { MdClear } from "react-icons/md"
 import { FaCalendarCheck } from "react-icons/fa"
 import { HabitTrackerRow } from "./HabitTrackerRow"
 import { PropTypes } from "react"
+import { Link } from 'react-router'
 
-export const HabitTrackerList = ({ days }) => (
-  <table>
-    <thead>
-      <tr>
-        <th>Date</th>
-        <th>Activity</th>
-        <th>Yes</th>
-        <th>No</th>
-      </tr>
-    </thead>
-    <tbody>{days.map((day, i) => <HabitTrackerRow key={i} {...day} />)}</tbody>
-  </table>
+export const HabitTrackerList = ({days, filter}) => {
+  const filteredDays = (!filter ||
+        !filter.match(/yes|no/))?
+        days:
+        days.filter(day => day[filter])
+  return (
+    <div className="habit-tracker-list">
+    <table>
+      <thead>
+        <tr>
+          <th>Date</th>
+          <th>Activity</th>
+          <th>Yes</th>
+          <th>No</th>
+        </tr>
+        <tr>
+            <td colSpan={4}>
+                <Link to="list-days">
+                  All Days
+                </Link>
+                <Link to="/list-days/yes">
+                  Yes Days
+                </Link>
+                <Link to="/list-days/no">
+                  No Days
+                </Link>
+            </td>
+        </tr>
+      </thead>
+      <tbody>
+        {filteredDays.map((day, i) => <HabitTrackerRow key={i} {...day} />)}</tbody>
+    </table>
+  </div>
 )
+}
 
 HabitTrackerList.propTypes = {
   days: function(props) {
